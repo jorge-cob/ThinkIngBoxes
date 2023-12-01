66239111b3ec01d691a64d7cd2c4bb7df2846893
surimi <jorge.cobreros@protonmail.com>
2023-12-01 13:35:50 +0100

    initial commit

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -1,6 +1,7 @@
 import { ExpoConfig, ConfigContext } from "@expo/config";
 
-const CLERK_PUBLISHABLE_KEY = "your-clerk-publishable-key";
+const CLERK_PUBLISHABLE_KEY =
+  "pk_test_ZWFzeS1saW9uZXNzLTY4LmNsZXJrLmFjY291bnRzLmRldiQ";
 
 const defineConfig = (_ctx: ConfigContext): ExpoConfig => ({
   name: "expo",
